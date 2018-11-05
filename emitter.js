97f5962d6a284136faa0bdba40763038cc5293d7
Ivan Plaksin <zero.netto.o@gmail.com>
2018-11-06 04:30:48 +0500

    Added solution

--- emitter.js
+++ emitter.js
@@ -6,11 +6,35 @@
  */
 const isStar = true;
 
+function handleEvent(contextsInfo) {
+    contextsInfo.forEach((handlersInfo, context) => {
+        handlersInfo.forEach(handlerInfo => {
+            wasSuccessApply(context, handlerInfo);
+        });
+    });
+}
+
+function wasSuccessApply(context, handlerInfo) {
+    const { handler, times, frequency, tryToApplyCount } = handlerInfo;
+    const shouldCall = (times === 0 || tryToApplyCount < times) &&
+        (frequency === 0 || tryToApplyCount % frequency === 0);
+
+    if (shouldCall) {
+        handler.apply(context);
+    }
+
+    handlerInfo.tryToApplyCount += 1;
+
+    return shouldCall;
+}
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const eventsInfo = new Map();
+
     return {
 
         /**
@@ -18,26 +42,64 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @param {Object} additionalInfo
+         * @returns {Object}
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (event, context, handler, additionalInfo = { times: 0, frequency: 0 }) {
+            const contextsInfo = eventsInfo.has(event) ? eventsInfo.get(event) : new Map();
+            eventsInfo.set(event, contextsInfo);
+
+            const handlersInfo = contextsInfo.has(context) ? contextsInfo.get(context) : [];
+            contextsInfo.set(context, handlersInfo);
+
+            handlersInfo.push({
+                handler,
+                times: additionalInfo.times,
+                frequency: additionalInfo.frequency,
+                tryToApplyCount: 0 });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            eventsInfo.forEach((eventInfo, eventName) => {
+                if (event === eventName) {
+                    eventsInfo.get(eventName).delete(context);
+                }
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            const events = [];
+            let path = '';
+            const splitedEvent = event.split('.');
+
+            splitedEvent.forEach((part, index) => {
+                path += part;
+                events.push(path);
+                path += index === splitedEvent.length - 1 ? '' : '.';
+            });
+
+            events.reverse().forEach(eventName => {
+                if (eventsInfo.has(eventName)) {
+                    handleEvent(eventsInfo.get(eventName));
+                }
+            });
+
+            return this;
         },
 
         /**
@@ -47,9 +109,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            this.on(event, context, handler, { times, frequency: 0 });
+
+            return this;
         },
 
         /**
@@ -59,9 +124,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            this.on(event, context, handler, { times: 0, frequency });
+
+            return this;
         }
     };
 }
